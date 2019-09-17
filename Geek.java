f868b615fb6efb1ea9ffba8a3a44d3640f825e29
Yukiao <lembang19sony@gmail.com>
2019-09-17 13:15:35 +0800

    Menambahkan 1 baris

--- Geek.java
+++ Geek.java
@@ -2,6 +2,8 @@ class Geek{
   public static void main(String[] args){
     for(int i=1; i<=4; i++){
           System.out.println("Contoh saja");
+  
     }
+    System.out.println("Cara pemakaian fungsi for");
   }
 }
